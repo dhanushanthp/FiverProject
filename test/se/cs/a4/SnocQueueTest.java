cba762844396bd470d0f4e4cdcbfba8658169538
Dhanushanth <root@dhanushanth-HP-ProBook-4430s>
2014-11-18 17:54:47 +0530

    adding

---
+++ test/se/cs/a4/SnocQueueTest.java
@@ -0,0 +1,29 @@
+package se.cs.a4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnocQueueTest {
+
+	@Test
+	public void testPeek() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDequeue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnqueue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+}
