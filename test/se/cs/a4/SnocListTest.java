45544d5c672e8652683631a94a5e9d75924e8f9e
Dhanushanth <dhanushanth.pushparajah@pearson.com>
2014-11-19 14:29:48 +0530

    fix

--- test/se/cs/a4/SnocListTest.java
+++ test/se/cs/a4/SnocListTest.java
@@ -11,46 +11,49 @@ public class SnocListTest {
 
 	@Before
 	public void setUp() {
-		charList =  new SnocList('a', new SnocList('b', new SnocList('c', null)));
+		charList = new SnocList('a', new SnocList('b', new SnocList('c', null)));
 	}
 
 	@Test
-	public void testSnocList(){
+	public void testSnocList() {
 		SnocList tmp = new SnocList('a', null);
 		assertEquals('a', tmp.getC());
 		assertEquals(null, tmp.getL());
 	}
-	
-	@Test
-	public void testCurrentNode(){
-		assertEquals('a',charList.getC());
-	}
-	
-	@Test
-	public void testNextNode(){
-		assertEquals('b',charList.getL().getC());
-	}
-	
-	@Test
-	public void testreverse(){
-		assertEquals("c,b,a,",charList.recursiveReverse(charList).toString());
-	}
-	
-	@Test
-	public void testNonreverse(){
-		assertNotEquals("b,c,a,",charList.recursiveReverse(charList).toString());
-	}
-	
-	@Test
-	public void testNextNodeReverse(){
-		assertEquals('b',(charList.recursiveReverse(charList)).getL().getC());
-	}
-	
-	@Test
-	public void testToString(){
-		assertEquals("a,b,c,",charList.toString());
-	}
-	
 
+	@Test
+	public void testCurrentNode() {
+		assertEquals('a', charList.getC());
+	}
 
+	@Test
+	public void testNextNode() {
+		assertEquals('b', charList.getL().getC());
+	}
+
+	@Test
+	public void testreverse() {
+		assertEquals("c,b,a,", charList.recursiveReverse(charList).toString());
+	}
+
+	@Test
+	public void testNonreverse() {
+		assertNotEquals("b,c,a,", charList.recursiveReverse(charList)
+				.toString());
+	}
+
+	@Test
+	public void testNextNodeReverse() {
+		assertEquals('b', (charList.recursiveReverse(charList)).getL().getC());
+	}
+
+	@Test
+	public void testNodeReverse() {
+		assertEquals('c', (charList.recursiveReverse(charList)).getC());
+	}
+
+	@Test
+	public void testToString() {
+		assertEquals("a,b,c,", charList.toString());
+	}
 }
